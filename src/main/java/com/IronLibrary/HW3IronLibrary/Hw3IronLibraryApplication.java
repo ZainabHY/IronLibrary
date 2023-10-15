9ac64d0269e866abe398b5ae1e844608032fc7fe
ZainabHY <zhalyousif@gmail.com>
2023-10-15 14:54:34 +0300

    first commit

---
+++ src/main/java/com/IronLibrary/HW3IronLibrary/Hw3IronLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.IronLibrary.HW3IronLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw3IronLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw3IronLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/IronLibrary/HW3IronLibrary/Hw3IronLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IronLibrary.HW3IronLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw3IronLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
