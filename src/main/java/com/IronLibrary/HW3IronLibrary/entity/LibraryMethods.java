465e54f6de5aaf8cf1ab7b94f80a661f6a75a885
ZainabHY <zhalyousif@gmail.com>
2023-10-17 10:08:57 +0300

    Adding LibraryMethods class

---
+++ src/main/java/com/IronLibrary/HW3IronLibrary/entity/LibraryMethods.java
@@ -0,0 +1,38 @@
+package com.IronLibrary.HW3IronLibrary.entity;
+
+
+public class LibraryMethods {
+
+    // 1. Add a book
+    public void addBook(){}
+
+    // 2. Search book by title
+    public void searchBookByTitle(){}
+
+    // 3. Search book by category
+    public void searchBookByCategory(){}
+
+    // 4. Search book by Author
+    public void searchBookByAuthor(){}
+
+    // 5. List all books along with author
+    public void listAllBooksWithAuthors(){}
+
+    // 6. Issue book to student
+    public void issueBookToStudent(){}
+
+    // 7. List books by USN
+    public void listAllBooksByUsn(){}
+
+    // 8. List books to be returned today
+    public void listBooksReturnedToday(){}
+
+    // 9. Book recommendation
+    public void bookRecommendation(){}
+
+    // 10. Update book information
+    public void updateBook(){}
+
+    // 11. Remove a book
+    public void removeBook(){}
+}
